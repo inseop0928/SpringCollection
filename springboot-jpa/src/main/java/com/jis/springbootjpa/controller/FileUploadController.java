43a378d7b0eb361ad94daba4bb8412c25ce091af
JenongInSeop <inseop0928@gmail.com>
2022-01-23 22:32:42 +0900

    fileupload add2

--- springboot-jpa/src/main/java/com/jis/springbootjpa/controller/FileUploadController.java
+++ springboot-jpa/src/main/java/com/jis/springbootjpa/controller/FileUploadController.java
@@ -10,25 +10,30 @@ import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.util.StreamUtils;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.util.UriUtils;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.Part;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @Controller
@@ -107,4 +112,40 @@ public class FileUploadController {
         return "redirect:/items/{itemId}";
     }
 
+    @GetMapping("/item/{id}")
+    public String itemView(@PathVariable Long id, Model model){
+
+        var fileItem = fileItemRepository.findById(id);
+        model.addAttribute("item",fileItem);
+        //return
+        return "item-view";
+    }
+
+    @ResponseBody
+    @GetMapping("/images/{filename}")
+    public Resource downloadImage(@PathVariable String fileName) throws MalformedURLException {
+        //파일을 반환
+        return new UrlResource("file" + fileStoreService.getFullPath(fileName));
+    }
+
+    @GetMapping("/attach/{itemId}")
+    public ResponseEntity<Resource> downloadAttach(@PathVariable Long id) throws MalformedURLException {
+        //findById getById
+        FileItem item = fileItemRepository.getById(id);
+        String storeFileName = item.getUploadFile().getFileKey();
+        String realFileName = item.getUploadFile().getRealFileName();
+
+        UrlResource resource = new UrlResource("file" + fileStoreService.getFullPath(storeFileName));
+
+        log.info("uploadFileName = {}",realFileName);
+        
+        //인코딩 설정
+        String encodeUploadFileName = UriUtils.encode(realFileName,StandardCharsets.UTF_8);
+        //파일 다운로드를 위한 설정
+        String contentDisposition = "attachment; filename=\"" + encodeUploadFileName +"\"";
+
+        return ResponseEntity.ok()
+             .header(HttpHeaders.CONTENT_DISPOSITION,contentDisposition)
+             .body(resource);
+    }
 }
